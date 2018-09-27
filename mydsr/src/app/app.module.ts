82487e1aef0decc1b24255f006cdc946cd15c395
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-09-27 23:05:40 +0530

    Updated List Componenet, still list.component.html not visible.

--- mydsr/src/app/app.module.ts
+++ mydsr/src/app/app.module.ts
@@ -8,38 +8,48 @@ import { CreateComponent } from './components/create/create.component';
 import { UpdateComponent } from './components/update/update.component';
 
 // 1) Import for routing
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 // 2) Import to use Angular Material Toolbar
-import {MatToolbarModule} from '@angular/material';
+import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatIconModule, MatButtonModule, MatCardModule, MatTableModule, MatDividerModule, MatSnackBarModule } from '@angular/material';
 // 3) Import to connect backend to frontend
-import {IssueService}  from './issue.service';
+import { IssueService } from './issue.service';
 
 // 1.1) Routing Configuration
 const routes: Routes = [
-  {path:'create', component: CreateComponent},
-  {path:'list',component:ListComponent},
-  {path:'edit/:id',component:UpdateComponent},
-  {path:'',redirectTo:'/list',pathMatch:'full'}
+    { path: 'create', component: CreateComponent },
+    { path: 'list', component: ListComponent },
+    { path: 'edit/:id', component: UpdateComponent },
+    { path: '', redirectTo: '/list', pathMatch: 'full' }
 ];
 
 // 1.2 RouterModule added in the @NgModule decorator to activate the routing
 // 2.1 Adding Material Toolbar to imports.
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent,
-    CreateComponent,
-    UpdateComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
-    MatToolbarModule
-  ],
-  providers: [
-    IssueService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListComponent,
+        CreateComponent,
+        UpdateComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(routes),
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatOptionModule,
+        MatSelectModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        MatTableModule,
+        MatDividerModule,
+        MatSnackBarModule
+    ],
+    providers: [
+        IssueService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- mydsr/src/app/components/list/list.component.ts
+++ mydsr/src/app/components/list/list.component.ts
@@ -1,22 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
 // Injecting Service
-import {IssueService} from '../../issue.service';
+import { IssueService } from '../../issue.service';
 import { Router } from '@angular/router';
+
+// Import for MatTable Data Source
+import { MatTableDataSource } from '@angular/material';
+import { Issue } from '../../issue.model';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
+// List Component Class
 export class ListComponent implements OnInit {
+
+  issues: Issue[];
+  displayedColumns = [
+    'title',
+    'responsible',
+    'description',
+    'severity',
+    'status'
+  ]
 
   constructor(
     private issueService: IssueService,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit() {
+    console.log("Fetching Issues!");
+    this.fetchIssues();
+  }
+
+  fetchIssues() {
+    this.issueService.getIssues()
+      .subscribe((data: Issue[]) => {
+        this.issues = data;
+        console.log('Data Requested...');
+        console.log('Data'+this.issues);
+      });
+  }
+
+  editIssue(id){
+    this.router.navigate([`/edit/${id}`]);
+  }
+
+  deleteIssue(id){
+    this.issueService.deleteIssue(id)
+    .subscribe(()=>{
+      this.fetchIssues();
+    });
   }
 
 }
